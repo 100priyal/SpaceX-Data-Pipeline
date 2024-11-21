811740e5bd0047e39ea41c339f78dd21a765a3fd
Priyal Kumar <1z1@vivaldi.net>
2024-11-21 06:34:46 +0530

    dbt configured

---
+++ data_manipulation.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+data = {
+	'rocket': [
+	'Falcon 1',
+	'Falcon 9',
+	'Falcon Heavy',
+	],
+	'launches': [5, 100, 3],
+}
+
+df = pd.DataFrame(data)
+
+# print(df['rocket'])
+
+# falcon9_df = df[df['rocket'] == 'Falcon 9']
+# print(falcon9_df)
+
+df['success_rate'] = [0.4, 0.98, 1.0]
+
+print(df)
